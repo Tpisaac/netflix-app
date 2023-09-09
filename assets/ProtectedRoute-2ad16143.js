0a1297e9b211448437416e4ceed6b640d3ef29e1
Temitope Ajilore <devtemi@outlook.com>
2023-09-09 06:08:53 +0100

    Updates

--- assets/ProtectedRoute-2ad16143.js
+++
@@ -1,1 +0,0 @@
-import{U as s,j as r,N as o}from"./index-71bcf27d.js";const u=({children:t})=>{const{user:e}=s();return e?t:r.jsx(o,{to:"/"})};export{u as default};
---
+++ assets/ProtectedRoute-34b3a49d.js
@@ -0,0 +1,1 @@
+import{U as s,j as r,N as o}from"./index-665bce17.js";const u=({children:t})=>{const{user:e}=s();return e?t:r.jsx(o,{to:"/"})};export{u as default};
